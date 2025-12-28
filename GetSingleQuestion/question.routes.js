956143bdcc88533b009e175104af347c2d3f5ec2
MohammedAliBelay <eng.mame17@gmail.com>
2025-12-28 23:43:37 +0300

    single question code is ready

--- GetSingleQuestion/question.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getQuestionById } from "./question.controller.js";
-
-const router = express.Router();
-
-router.get("/question/:question_id", getQuestionById);
-
-export default router;
