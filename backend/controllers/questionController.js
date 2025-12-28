28e6074443c04b4a7bb11b0a588cbce82e008471
MohammedAliBelay <eng.mame17@gmail.com>
2025-12-28 23:37:47 +0300

    all done

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -1,21 +1,38 @@
-import { getAllQuestions as getAllQuestionsModel } from "../models/questionModel.js";
-
+import db from "../config/database.js";
 
 /**
  * Get all questions
  */
 export const getAllQuestions = async (req, res) => {
   try {
-    const questions = await getAllQuestionsModel();
+    // 1. Fetch all questions from the database
+    const [rows] = await db.promise().query("SELECT * FROM questions");
+
+    // 2. Send response
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+/** get question by ID */
+export const getQuestionById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [row] = await db
+      .promise()
+      .query("SELECT * FROM questions WHERE question_id = ?", [id]);
+
+    if (row.length === 0) {
+      return res.status(404).json({
+        message: "Question not found",
+      });
+    }
 
     res.status(200).json({
-      success: true,
-      total: questions.length,
-      data: questions,
+      data: row[0],
     });
   } catch (error) {
     res.status(500).json({
-      success: false,
       message: "Internal Server Error",
     });
   }
--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { getAllQuestions } from "../controllers/questionController.js";
+import { getQuestionById } from "../controllers/questionController.js";
 
 const router = express.Router();
 
 // Public routes
 router.get("/", getAllQuestions);
+router.get("/:id", getQuestionById);
 
 export default router;
