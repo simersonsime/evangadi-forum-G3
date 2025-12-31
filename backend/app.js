39af9d23cbd763fa5468818eed83b7a3f5d3028f
Ahlam <ahlam.ahmed@aastustudent.edu.et>
2025-12-31 23:33:12 +0300

    update signup

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 // Import routes
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routes/authRouter.js";
 import questionRoutes from "./routes/questionRoutes.js";
 // import answerRoutes from "./routes/answerRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
@@ -11,12 +11,13 @@ dotenv.config();
 const app = express();
 
 // -----------Middleware------------//
-app.use(express.json()); //  MUST BE BEFORE ROUTES
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
 // ----------Routes--------------//
 app.use("/api/user", authRoutes);
 app.use("/api/question", questionRoutes);
-app.use("/api/answer", answerRoutes);
+// app.use("/api/answer", answerRoutes);
 app.use("/api/comments", commentRoutes);
 
 // ----------Default route----------//
