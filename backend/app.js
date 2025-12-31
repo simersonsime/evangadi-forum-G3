351e9a118caf609a3ec61f83ab5979a0d7a52113
Ahlam <ahlam.ahmed@aastustudent.edu.et>
2025-12-31 22:31:03 +0300

    add signup features

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 // Import routes
-// import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import questionRoutes from "./routes/questionRoutes.js";
 // import answerRoutes from "./routes/answerRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
@@ -15,7 +15,7 @@ app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 // ----------Routes--------------//
-// app.use("/api/user", authRoutes);
+app.use("/api/user", authRoutes);
 app.use("/api/question", questionRoutes);
 // app.use("/api/answer", answerRoutes);
 app.use("/api/comments", commentRoutes);
