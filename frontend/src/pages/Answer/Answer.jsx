4838f695c2a93245a7e03787777c173f6be2e9a6
MohammedAliBelay <eng.mame17@gmail.com>
2026-01-06 23:08:11 +0300

    updated

--- frontend/src/pages/Answer/Answer.jsx
+++ frontend/src/pages/Answer/Answer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { FaUserCircle } from "react-icons/fa";
 import "./Answer.css";
