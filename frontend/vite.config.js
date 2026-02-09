d7d6646a08197d7650d1d0d41ce75707d0cb69a8
Simerson Sime <simersonsime1@gmail.com>
2026-02-09 21:57:40 +0300

    fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: true,
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,
+  },
+});
